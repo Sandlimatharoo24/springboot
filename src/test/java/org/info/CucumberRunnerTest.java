951ac01cda454097a04046620a742a2d3956e4a7
Amit Kumar Chaudhary <amitchaudhary.it@gmail.com>
2017-05-14 22:45:23 +0530

    added

--- src/test/java/org/info/CucumberRunnerTest.java
+++ src/test/java/org/info/CucumberRunnerTest.java
@@ -8,7 +8,7 @@ import cucumber.api.junit.Cucumber;
 @CucumberOptions(plugin = { "html:target/output/LoginReport", "json:target/output/cucumber.json", 
 		"pretty:target/output/cucumber-pretty.txt", "usage:target/output/cucumber-usage.json",
 		"junit:target/output/cucumber-results.xml" }, 
-features = "classpath:features/cucumberJava.feature", glue = {"classpath: stepDefinition"})
+features = "src/test/javafeatures/cucumbava.feature", glue = {"stepdefinition"})
 public class CucumberRunnerTest {
 
 }
--- src/test/java/stepdefinition/StepdefNew.java
+++ src/test/java/stepdefinition/StepdefNew.java
@@ -1,4 +1,4 @@
-package stepDefinition;
+package stepdefinition;
 
 import static org.junit.Assert.assertTrue;
 
