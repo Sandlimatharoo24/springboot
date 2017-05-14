0bab1ed0e496623008a0b3c17567ceb3dc205b3f
Amit Kumar Chaudhary <amitchaudhary.it@gmail.com>
2017-05-14 22:49:33 +0530

    added new

--- src/test/java/org/info/CucumberRunnerTest.java
+++ src/test/java/org/info/CucumberRunnerTest.java
@@ -5,10 +5,10 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = { "html:target/output/LoginReport", "json:target/output/cucumber.json", 
-		"pretty:target/output/cucumber-pretty.txt", "usage:target/output/cucumber-usage.json",
-		"junit:target/output/cucumber-results.xml" }, 
-features = "src/test/javafeatures/cucumbava.feature", glue = {"stepdefinition"})
+@CucumberOptions(plugin = { "html:src/test/output/LoginReport", "json:src/test/output/cucumber.json", 
+		"pretty:src/test/output/cucumber-pretty.txt", "usage:src/test/output/cucumber-usage.json",
+		"junit:src/test/output/cucumber-results.xml" }, 
+features = "src/test/java/features/cucumberJava.feature", glue = {"stepDefinition"})
 public class CucumberRunnerTest {
 
 }
--- src/test/java/stepdefinition/StepdefNew.java
+++ src/test/java/stepdefinition/StepdefNew.java
@@ -1,4 +1,4 @@
-package stepdefinition;
+package stepDefinition;
 
 import static org.junit.Assert.assertTrue;
 
