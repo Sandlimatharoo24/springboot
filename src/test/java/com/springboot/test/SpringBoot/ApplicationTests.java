56eb4684c246bed8d416231333d8918c14ea6b95
DESKTOP-7JRTRPT\Sandli <sandlimatharoo24@gmail.com>
2017-05-14 19:47:15 +0530

    updated

---
+++ src/test/java/com/springboot/test/SpringBoot/ApplicationTests.java
@@ -0,0 +1,26 @@
+package com.springboot.test.SpringBoot;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@Ignore
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTests {
+
+	@Test
+	public void craxySandli() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void crazyAmit() {
+		assertTrue(true);
+	}
+
+}
---
+++ src/test/java/com/springboot/test/SpringBoot/ServletInitializerTest.java
@@ -0,0 +1,19 @@
+package com.springboot.test.SpringBoot;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ServletInitializerTest {
+
+	@Test
+	public void craxySandli() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void crazyAmit() {
+		assertTrue(true);
+	}
+
+}
